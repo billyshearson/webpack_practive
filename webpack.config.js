e1c7a687a498aaea916bf42e2fe096b7f035f420
billyshears <63904750+billyshearson@users.noreply.github.com>
2020-07-27 13:22:32 +0900

    html-loader and pug

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,19 @@ module.exports = {
                     },
                 }, ],
             },
+            {
+                test: /\.pug/,
+                use: [{
+                        loader: "html-loader",
+                    },
+                    {
+                        loader: "pug-html-loader",
+                        options: {
+                            pretty: true,
+                        },
+                    },
+                ],
+            },
         ],
     },
     plugins: [
@@ -37,7 +50,12 @@ module.exports = {
             filename: "./stylesheets/main.css",
         }),
         new HtmlWebpackPlugin({
-            template: "./src/templates/index.html",
+            template: "./src/templates/index.pug",
+            filename: "index.html",
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/templates/access.pug",
+            filename: "access.html",
         }),
         new CleanWebpackPlugin(),
     ],
