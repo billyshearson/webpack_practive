deace187a498be4a8a453081be0067e6bcab7781
billyshears <63904750+billyshearson@users.noreply.github.com>
2020-08-01 09:00:37 +0900

    refactoring sass

--- webpack.config.js
+++ webpack.config.js
@@ -55,6 +55,7 @@ module.exports = {
                         options: {
                             esModule: false,
                             name: "images/[name].[ext]",
+                            publicPath: "/",
                         },
                     },
                     {
