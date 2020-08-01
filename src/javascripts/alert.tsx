35581f8998d5a9ce3400f36c021fc0fded4b0a7d
billyshears <63904750+billyshearson@users.noreply.github.com>
2020-08-01 23:19:10 +0900

    react styled-components

--- src/javascripts/alert.tsx
+++ src/javascripts/alert.tsx
@@ -1,7 +1,19 @@
 import * as React from "react";
+import styled from "styled-components";
+
+const AlertContainer = styled.div`
+  background-color: green;
+  color: #fff;
+  padding: 1em;
+  border-radius: 4em;
+`;
 
 const Alert: React.FC<{ message: string }> = ({ message }) => {
-  return <div>{message}</div>;
+  return (
+    <AlertContainer>
+      <div>{message}</div>
+    </AlertContainer>
+  );
 };
 
 export default Alert;
